629f90185cc8813b3a1eddbb7d81501e0dec83e1
Suwat Inkaew <swik999@gmail.com>
2021-01-19 16:09:06 +0700

    done

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,42 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+	double sum = 0;
+	for (int i = 0; i < N;i++){
+		sum += A[i];
+	}
+	B[0] = sum/N;
+	
+	double SD,D;
+	for (int i = 0; i < N;i++){
+		D += pow(A[i] - B[0], 2);
+	}
+	SD = sqrt(D/N);
+	B[1] = SD;
+	
+	double s = 1.0;
+	for(int i = 0; i < N ;i++){
+		s *= A[i];
+	}
+	double GM;
+	GM = pow(s,(1.0/N));
+	B[2] = GM;
+	
+	double ss = 0;
+	for(int i = 0; i < N ;i++){
+		ss = ss+(1/A[i]);
+	}
+	double HM;
+	HM = N * pow(ss,-1);
+	B[3] = HM;
+	
+	double MAX = A[0],MIN = A[0];
+	for(int i = 1; i < N; i++){
+		if(A[i] > MAX) MAX = A[i];
+		if(A[i] < MIN) MIN = A[i];
+	}
+	B[4]=MAX;
+	B[5]=MIN;
+}
